1da3ca4b11cd71e4f6a03fc5db53f8e033bb4b40
Andrew Abishek Azhagesan <44633226+AndyGamer3699@users.noreply.github.com>
2019-11-25 17:54:15 +0530

    UI and change

--- app/src/main/java/com/example/areaalert/MainActivity.java
+++ app/src/main/java/com/example/areaalert/MainActivity.java
@@ -97,6 +97,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        hideNavigationBar();
+
         authorities = getApplicationContext().getPackageName() + ".fileprovider";
         date = new Date();
 
@@ -452,4 +454,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
 
     }
+
+    public void hideNavigationBar(){
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                );
+    }
+
 }
